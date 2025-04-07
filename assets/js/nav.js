931e6b85395127626a9ac693e8f1d64fa4dff473
Mahib Abrar <128323013+MahibAbrar222@users.noreply.github.com>
2025-04-07 10:28:29 +0000

    Add background image to main section on page load for enhanced visual appeal

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,3 +5,8 @@ menu_bar.addEventListener('click', () => {
     menu_bar.classList.toggle('active');
     menu_items.classList.toggle('active');
 });
+
+// When fully load the page\
+window.addEventListener('load', () => {
+    document.querySelector("#main").style.background = "url('/assets/images/islamic-background-2.png')";
+});
